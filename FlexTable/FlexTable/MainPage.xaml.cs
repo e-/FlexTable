bb9033718e152273affd50a06d356192a7746911
Jaemin Jo <jmjo@hcil.snu.ac.kr>
2015-09-22 15:36:40 +0900

    to uwp

---
+++ FlexTable/FlexTable/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 빈 페이지 항목 템플릿은 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 에 문서화되어 있습니다.
+
+namespace FlexTable
+{
+    /// <summary>
+    /// 자체에서 사용하거나 프레임 내에서 탐색할 수 있는 빈 페이지입니다.
+    /// </summary>
+    public sealed partial class MainPage : Page, IMainPage
+    {
+        ViewModel.MainPageViewModel mainPageViewModel;
+        Util.CsvLoader csvLoader = new Util.CsvLoader();
+
+        public View.TableView TableView { get { return TableViewElement; } }
+        public View.ExplorationView ExplorationView { get { return ExplorationViewElement; } }
+        public TextBlock DummyTextBlock { get { return DummyCell; } }
+
+        public MainPage()
+        {
+            mainPageViewModel = new ViewModel.MainPageViewModel(this);
+            this.DataContext = mainPageViewModel;
+            this.InitializeComponent();
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            var view = ApplicationView.GetForCurrentView();
+
+            view.TryEnterFullScreenMode();
+
+            Model.Sheet sheet = await csvLoader.Load();
+            mainPageViewModel.Sheet = sheet;
+
+            mainPageViewModel.Initialize();
+        }
+    }
+}
